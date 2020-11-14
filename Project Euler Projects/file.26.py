f9f35b56f9cacb041e89a22fdbd4004038d4fca7
apexx77 <hi2asifahmad@gmail.com>
2020-11-14 11:31:39 +0530

    Updated Reciprocal Cycles(26)

--- Project Euler Projects/file.26.py
+++ Project Euler Projects/file.26.py
@@ -1,1 +1,7 @@
-print(1/9)
+def task(n, d):
+    for i in range(1, d):
+        if 1 == 10 ** i % d:
+            return i
+    return 0
+longest = max(task(1, i) for i in range(2, 1001))
+print([i for i in range(2, 1001) if task(1, i) == longest][0])
